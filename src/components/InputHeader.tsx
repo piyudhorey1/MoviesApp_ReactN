0b6725df3c1900ee74e4bee30a12f6e58d589750
Piyushdhorey <pdhorey@cxr.agency>
2024-02-14 16:07:41 +0530

    Added loader, search bar component

---
+++ src/components/InputHeader.tsx
@@ -0,0 +1,65 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcons from './CustomIcons';
+
+const InputHeader = (props: any) => {
+  const [searchText, setSearchText] = useState('');
+  return (
+    <View style={styles.inputBox}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={textInput => setSearchText(textInput)}
+        value={searchText}
+        placeholder="Search your movies"
+        placeholderTextColor={COLORS.WhiteRGBA32}
+      />
+      <TouchableOpacity
+        style={styles.customIcon}
+        onPress={() => props.searchFunction(searchText)}>
+        <CustomIcons
+          name="search"
+          color={COLORS.Orange}
+          size={FONTSIZE.size_14}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputBox: {
+    display: 'flex',
+    paddingVertical: SPACING.space_8,
+    paddingHorizontal: SPACING.space_20,
+    borderWidth: 2,
+    borderColor: COLORS.WhiteRGBA15,
+    borderRadius: BORDERRADIUS.radius_25,
+    flexDirection: 'row',
+  },
+  textInput: {
+    width: '90%',
+    color: COLORS.White,
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+  },
+  customIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.space_10,
+  },
+});
+
+export default InputHeader;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,24 +1,75 @@
-import {Text, StyleSheet, View, TouchableOpacity} from 'react-native';
-import React, {Component} from 'react';
-import {COLORS} from '../theme/theme';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  ScrollView,
+  StatusBar,
+  ActivityIndicator,
+} from 'react-native';
+import React, {Component, useState} from 'react';
+import {COLORS, SPACING} from '../theme/theme';
+import {
+  upcomingMovies,
+  popularMovies,
+  nowPlayingMovies,
+  baseImagePath,
+} from '../api/ApiCalls';
+import InputHeader from '../components/InputHeader';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const {width, height} = Dimensions.get('window');
 
 const HomeScreen = ({navigation}: any) => {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.push('MovieDetails');
-        }}>
-        <Text>HomeScreen</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  const [nowPlayingMoviesList, setNotPlayingMoviesList] = useState(undefined);
+  const [popularMoviesList, setPopularMoviesList] = useState(undefined);
+  const [upcomingMoviesList, setUpcomingMoviesList] = useState(undefined);
+
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviesList == undefined &&
+    popularMoviesList == null &&
+    upcomingMoviesList == undefined &&
+    upcomingMoviesList == null
+  ) {
+    return (
+      <ScrollView
+        style={styles.container}
+        bounces={false}
+        contentContainerStyle={styles.scrollViewContainer}>
+        <SafeAreaView>
+          <StatusBar hidden />
+
+          <View style={styles.InputHeaderContainer}>
+            <InputHeader />
+          </View>
+        </SafeAreaView>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    );
+  }
+  return <View style={styles.container}></View>;
 };
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.Black,
-    marginTop: 90,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  InputHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
   },
 });
 
